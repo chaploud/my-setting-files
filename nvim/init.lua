35a08cecb196a16984fc7228c64cf77d2969879b
Shota Kudo <in-the-cloud.950@hotmail.co.jp>
2025-04-23 23:33:47 +0900

    smartcase指定漏れ

--- nvim/init.lua
+++ nvim/init.lua
@@ -124,6 +124,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
