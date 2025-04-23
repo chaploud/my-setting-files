646535b5a224b7a7ed254e4ec6af0277615736f1
Shota Kudo <in-the-cloud.950@hotmail.co.jp>
2025-04-23 18:06:48 +0900

    surround-nvim導入

--- nvim/init.lua
+++ nvim/init.lua
@@ -106,3 +106,37 @@ vim.cmd([[
     return ''
   endfunction
 ]])
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    {
+      -- カッコなどの包む系の操作を追加
+      "kylechui/nvim-surround",
+      version = "^3.0.0",
+      event = "VeryLazy",
+      config = function()
+        require("nvim-surround").setup({})
+      end
+    }
+  },
+  install = {},
+  checker = { enabled = false },
+})
