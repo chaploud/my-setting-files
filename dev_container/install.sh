cc836d5695a31413b66a37a9166d14ddace63821
Shota Kudo (chaploud) <shota.508@studist.jp>
2025-07-13 21:22:16 +0900

    commit

--- dev_container/install.sh
+++ dev_container/install.sh
@@ -2,25 +2,27 @@
 # 開発コンテナ内になるべく自分の環境を再現
 # ホストPCでSSHキーエージェント転送していることを前提とする
 
+DOT=$HOME/dotfiles
+DEV=$DOT/dev_container
+
 # CLIツール
 apt install -y tree jq ripgrep fzf neovim
 
 # starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
-mkdir -p ~/.config
-cp ../starship/starship.toml ~/.config/starship.toml
+mkdir -p $HOME/.config
+cp $DOT/starship/starship.toml $HOME/.config/starship.toml
 
 # bashrc
-echo ./bashrc >> ~/.bashrc
+echo $DEV/bashrc >> $HOME/.bashrc
 
 # VSCode拡張機能
 if [ -f ./extensions.txt ]; then
-  cat ./extensions.txt | xargs -L 1 code --install-extension
+  cat $DEV/extensions.txt | xargs -L 1 code --install-extension
 fi
 
-# npxがあれば終了
-if command -v npx &> /dev/null; then
-  echo "npx is already installed."
+if command -v npm &> /dev/null; then
+  echo "npm is already installed."
 else
   curl https://get.volta.sh | bash
   export VOLTA_HOME="$HOME/.volta"
