3104bd9dc9f7d2078125c3590971ec8140acbdc1
Shota Kudo (chaploud) <shota.508@studist.jp>
2025-07-15 11:52:47 +0900

    commit

--- dev_container/install.sh
+++ dev_container/install.sh
@@ -16,15 +16,3 @@ cp $DOT/starship/starship.toml $HOME/.config/starship.toml
 
 # bashrc
 cat $DEV/bashrc >> $HOME/.bashrc
-
-if command -v npm &> /dev/null; then
-  echo "npm is already installed."
-else
-  curl https://get.volta.sh | bash
-  export VOLTA_HOME="$HOME/.volta"
-  export PATH="$VOLTA_HOME/bin:$PATH"
-  $VOLTA_HOME/bin/volta install node@22
-fi
-
-# claude code
-npm install -g @anthropic-ai/claude-code
