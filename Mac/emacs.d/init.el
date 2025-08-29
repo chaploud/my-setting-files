da24827a6d6b9f0634ab5952071c9811f5c7f4a6
Shota Kudo (chaploud) <shota.508@studist.jp>
2025-08-29 18:08:23 +0900

    修正

--- Mac/emacs.d/init.el
+++ Mac/emacs.d/init.el
@@ -21,7 +21,6 @@
 ;; (7) clojure-lsp: `brew install clojure-lsp/brew/clojure-lsp-native`
 ;; (8) docker/orbstack (コンテナを使うなら適宜インストール)
 ;; (9) Tree-sitterをコンパイルできるもの: `xcode-select --install`
-;; (10) npm install -g @github/copilot-language-server
 
 ;; === 必要フォント
 ;; Source Han Code JP (https://github.com/adobe-fonts/source-han-code-jp)
@@ -58,8 +57,8 @@
 ;; === 初回のEmacs起動後に必要なコマンド
 ;; M-x nerd-icons-install-fonts (nerd-iconsのフォントをインストール)
 ;; M-x eat-compile-terminfo (Macでeatターミナルの操作不具合を防止)
-;; M-x copilot-install-serverが必要
-;; M-x treesit-install-language-grammar
+;; M-x copilot-install-server (GitHub Copilotのサーバー)
+;; M-x treesit-install-language-grammar (高速な構文解析)
 ;;   yaml, https://github.com/ikatyang/tree-sitter-yaml, 後はデフォルト
 ;;   json, https://github.com/tree-sitter/tree-sitter-json, 後はデフォルト
 
@@ -1484,14 +1483,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((claude-code-ide :url
-                      "https://github.com/manzaltu/claude-code-ide.el")
-     (copilot :url "https://github.com/copilot-emacs/copilot.el"
-              :branch "main")
-     (eat :url "http://codeberg.org/akib/emacs-eat")))
- '(safe-local-variable-directories '("/Users/shota.508/Studist/teachme_eboshigara/")))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
